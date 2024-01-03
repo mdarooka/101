497c6e793d1aa28b496dbabf598ca5db8bd16ab9
mdarooka <manavdarooka1@gmail.com>
2024-01-03 13:50:58 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import './globals.css';
+
+function App() {
+    return (
+        <main className='flex h-screen'>
+            
+        </main> 
+    )
+}
+
+export default App

260fb3c55422851178acb15ef80766e016080b7a
mdarooka <manavdarooka1@gmail.com>
2024-01-03 16:34:47 -0800

    added tailwind, creating signin and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import './globals.css';
 
 function App() {
     return (
         <main className='flex h-screen'>
-            
-        </main> 
+            <Routes>
+                {/* public routes */}
+                <Route>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-up' element={<SignupForm />} />
+                </Route>
+                {/* private routes */}
+                <Route>
+
+                </Route>
+            </Routes>
+        </main>
     )
 }
 
